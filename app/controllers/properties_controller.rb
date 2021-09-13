105b2d3d2ab5d6569c858087236ad19be559c53b
elgnonvis <elgnonvis@gmail.com>
2021-09-13 11:17:47 +0100

    Generate Station's model

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -64,6 +64,6 @@ class PropertiesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def property_params
-      params.require(:property).permit(:name, :rent, :age, :address, :remark)
+      params.require(:property).permit(:name, :rent, :age, :address, :remark, stations_attributes: [:id, :railway, :name, :walk])
     end
 end
